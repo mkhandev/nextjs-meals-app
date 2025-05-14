f13e43a953f360b13cbe793972e7984c8f940654
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-05-14 16:36:46 +0600

    add next js app

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+function Error() {
+  return (
+    <div className="error">
+      <h1>An error occurred!</h1>
+      <p>Faild to fetch meals data. Please try again letter.</p>
+    </div>
+  );
+}
+
+export default Error;
