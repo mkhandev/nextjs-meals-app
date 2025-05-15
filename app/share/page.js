080878e2216e1547220996d7c89c1c701f3087ee
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-05-15 11:40:17 +0600

    add image share

--- app/share/page.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ShareMealPage() {
-  return <div>Share meal page</div>;
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // or higher if needed
+    },
+  },
+};
 
 export default nextConfig;
